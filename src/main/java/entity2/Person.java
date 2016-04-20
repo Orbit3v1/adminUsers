c6b1867bb23f2ced29c9e740a646ede81b76cdf4
ayaroslavtsev <duke007@ukr.net>
2016-04-20 16:10:18 +0300

    file rename

---
+++ src/main/java/entity2/Person.java
@@ -0,0 +1,97 @@
+package entity2;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "person")
+public class Person {
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_Name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "state")
+    private String state;
+
+    @ManyToMany
+    @JoinTable(name = "person_role",
+        joinColumns = @JoinColumn(name = "person"),
+        inverseJoinColumns = @JoinColumn(name = "role"))
+    private List<Role> roles;
+
+    public Person() {
+    }
+
+    public Person(String firstName, String lastName, String email, String state) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.state = state;
+    }
+
+    public Person(String firstName, String lastName, String email, String state, List<Role> roles) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.state = state;
+        this.roles = roles;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+}
---
+++ src/main/java/entity2/Role.java
@@ -0,0 +1,75 @@
+package entity2;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @ManyToMany
+    @JoinTable(name = "role_privilege_action",
+            joinColumns = @JoinColumn(name = "role"),
+            inverseJoinColumns = {
+                    @JoinColumn(name = "privilege", referencedColumnName = "privilege"),
+                    @JoinColumn(name = "action", referencedColumnName = "action")
+            })
+    private List<PrivilegeAction> privilegeAction;
+
+    public Role() {
+    }
+
+    public Role(String id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Role(String id, String name, String description, List<PrivilegeAction> privilegeAction) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.privilegeAction = privilegeAction;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<PrivilegeAction> getPrivilegeAction() {
+        return privilegeAction;
+    }
+
+    public void setPrivilegeAction(List<PrivilegeAction> privilege) {
+        this.privilegeAction = privilege;
+    }
+}
