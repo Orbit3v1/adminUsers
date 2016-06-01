7f6db258d38d6936f52263ea278a33db09475343
ayaroslavtsev <duke007@ukr.net>
2016-06-01 15:30:13 +0300

    add unit test for RoleValidator

--- src/main/java/com/app/validator/RoleValidator.java
+++ src/main/java/com/app/validator/RoleValidator.java
@@ -10,42 +10,39 @@ import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import java.util.List;
 import java.util.ResourceBundle;
 import java.util.regex.Pattern;
 
 @Named("roleValidator")
 @Scope("request")
-public class RoleValidator extends AbstractValidator<Role>{
+public class RoleValidator extends AbstractValidator<Role> {
 
-    private boolean edit;
+    protected boolean edit;
 
     public RoleValidator() {
     }
 
     @Override
-    public boolean validate(Role role, Object ... args) {
+    public boolean validate(Role role, Object... args) {
         this.entity = role;
         edit = args.length > 0 ? (Boolean) args[0] : false;
         return isValidId() & isValidName();
     }
 
-    private boolean isValidId() {
+    protected boolean isValidId() {
         boolean valid = true;
         Pattern pattern = Pattern.compile("[^a-zA-Z0-9_-]+");
         String errorMessage = "";
-        if (entity.getId().equals("")) {
+        if (entity.getId() == null || entity.getId().equals("")) {
             valid = false;
             errorMessage = "error.notNull";
         } else if (pattern.matcher(entity.getId()).find()) {
             valid = false;
             errorMessage = "roleScreen.error.idPattern";
-        } else if (!edit) {
-            Query query = em.createQuery("select r from Role r where r.id = :id")
-                    .setParameter("id", entity.getId());
-            if (query.getResultList().size() != 0) {
-                valid = false;
-                errorMessage = "roleScreen.error.idDuplicate";
-            }
+        } else if (!edit && getRolesWithSameId().size() != 0) {
+            valid = false;
+            errorMessage = "roleScreen.error.idDuplicate";
         }
         if (!valid) {
             addMessage.setMessage("mainForm:id", errorMessage, FacesMessage.SEVERITY_ERROR);
@@ -53,21 +50,29 @@ public class RoleValidator extends AbstractValidator<Role>{
         return valid;
     }
 
-    private boolean isValidName() {
+    protected List<Role> getRolesWithSameId() {
+        Query query = em.createQuery("select r from Role r where r.id = :id")
+                .setParameter("id", entity.getId());
+        return query.getResultList();
+    }
+
+    protected boolean isValidName() {
         boolean valid = true;
-        if (entity.getName().equals("")) {
+        if (entity.getName() == null || entity.getName().equals("")) {
             valid = false;
             addMessage.setMessage("mainForm:name", "error.notNull", FacesMessage.SEVERITY_ERROR);
-        } else {
-            Query query = em.createQuery("select r from Role r where r.name = :name and r.id != :id")
-                    .setParameter("name", entity.getName())
-                    .setParameter("id", edit ? entity.getId() : "");
-            if (query.getResultList().size() != 0) {
-                valid = false;
-                addMessage.setMessage("mainForm:name", "roleScreen.error.nameDuplicate", FacesMessage.SEVERITY_ERROR);
-            }
+        } else if (getRolesWithSameName().size() != 0) {
+            valid = false;
+            addMessage.setMessage("mainForm:name", "roleScreen.error.nameDuplicate", FacesMessage.SEVERITY_ERROR);
         }
         return valid;
+    }
+
+    protected List<Role> getRolesWithSameName() {
+        Query query = em.createQuery("select r from Role r where r.name = :name and r.id != :id")
+                .setParameter("name", entity.getName())
+                .setParameter("id", edit ? entity.getId() : "");
+        return query.getResultList();
     }
 
 }
---
+++ src/test/java/com/app/validator/RoleValidatorTest.java
@@ -0,0 +1,169 @@
+package com.app.validator;
+
+import com.app.entity.Person;
+import com.app.entity.Role;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+
+public class RoleValidatorTest {
+
+    RoleValidator validator;
+
+    @Before
+    public void init(){
+        validator = new RoleValidator();
+        validator.addMessage = (componentId, bundleKey, type) -> {};
+    }
+
+    @Test
+    public void isValidIdOk() {
+
+        Role role = new Role();
+        role.setId("Test_123");
+        validator.entity = role;
+
+        List<Role> rolesWithSameId = new ArrayList<>();
+
+        RoleValidator spy = Mockito.spy(validator);
+        Mockito.doReturn(rolesWithSameId).when(spy).getRolesWithSameId();
+
+        boolean result = spy.isValidId();
+        assertTrue(result);
+    }
+
+    @Test
+    public void isValidIdWrongCharacters() {
+
+        Role role = new Role();
+        role.setId("Test_123!б");
+        validator.entity = role;
+
+        List<Role> rolesWithSameId = new ArrayList<>();
+
+        RoleValidator spy = Mockito.spy(validator);
+        Mockito.doReturn(rolesWithSameId).when(spy).getRolesWithSameId();
+
+        boolean result = spy.isValidId();
+        assertFalse(result);
+    }
+
+    @Test
+    public void isValidIdEmpty() {
+
+        Role role = new Role();
+        role.setId("");
+        validator.entity = role;
+
+        List<Role> rolesWithSameId = new ArrayList<>();
+
+        RoleValidator spy = Mockito.spy(validator);
+        Mockito.doReturn(rolesWithSameId).when(spy).getRolesWithSameId();
+
+        boolean result = spy.isValidId();
+        assertFalse(result);
+    }
+
+    @Test
+    public void isValidIdNull() {
+
+        Role role = new Role();
+        validator.entity = role;
+
+        List<Role> rolesWithSameId = new ArrayList<>();
+
+        RoleValidator spy = Mockito.spy(validator);
+        Mockito.doReturn(rolesWithSameId).when(spy).getRolesWithSameId();
+
+        boolean result = spy.isValidId();
+        assertFalse(result);
+    }
+
+    @Test
+    public void isValidIdSameExists() {
+
+        Role role = new Role();
+        role.setId("Test");
+        validator.entity = role;
+
+        List<Role> rolesWithSameId = new ArrayList<>();
+        rolesWithSameId.add(new Role());
+
+        RoleValidator spy = Mockito.spy(validator);
+        Mockito.doReturn(rolesWithSameId).when(spy).getRolesWithSameId();
+
+        boolean result = spy.isValidId();
+        assertFalse(result);
+    }
+
+    @Test
+    public void isValidNameOk() {
+
+        Role role = new Role();
+        role.setName("Test");
+        validator.entity = role;
+
+        List<Role> rolesWithSameName = new ArrayList<>();
+
+        RoleValidator spy = Mockito.spy(validator);
+        Mockito.doReturn(rolesWithSameName).when(spy).getRolesWithSameName();
+
+        boolean result = spy.isValidName();
+        assertTrue(result);
+    }
+
+    @Test
+    public void isValidNameEmpty() {
+
+        Role role = new Role();
+        role.setName("");
+        validator.entity = role;
+
+        List<Role> rolesWithSameName = new ArrayList<>();
+
+        RoleValidator spy = Mockito.spy(validator);
+        Mockito.doReturn(rolesWithSameName).when(spy).getRolesWithSameName();
+
+        boolean result = spy.isValidName();
+        assertFalse(result);
+    }
+
+    @Test
+    public void isValidNameNull() {
+
+        Role role = new Role();
+        validator.entity = role;
+
+        List<Role> rolesWithSameName = new ArrayList<>();
+
+        RoleValidator spy = Mockito.spy(validator);
+        Mockito.doReturn(rolesWithSameName).when(spy).getRolesWithSameName();
+
+        boolean result = spy.isValidName();
+        assertFalse(result);
+    }
+
+    @Test
+    public void isValidNameSameExists() {
+
+        Role role = new Role();
+        role.setName("Test");
+        validator.entity = role;
+
+        List<Role> rolesWithSameName = new ArrayList<>();
+        rolesWithSameName.add(new Role());
+
+        RoleValidator spy = Mockito.spy(validator);
+        Mockito.doReturn(rolesWithSameName).when(spy).getRolesWithSameName();
+
+        boolean result = spy.isValidName();
+        assertFalse(result);
+    }
+}
