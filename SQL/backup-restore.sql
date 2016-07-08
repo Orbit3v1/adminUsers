853c734e2b9bea8fac093ed46c9940e53ae5d956
ayaroslavtsev <duke007@ukr.net>
2016-07-08 11:09:39 +0300

    add backup/restore script change names of sql scripts - add order of deployment

---
+++ SQL/backup-restore.sql
@@ -0,0 +1,19 @@
+--backup
+BACKUP DATABASE [appOwner]
+TO DISK = 'C:\work\backup\appOwner.bak'
+  WITH FORMAT,
+    MEDIANAME = 'MyBackups',
+    NAME = 'Full backup of my database'
+
+
+--restore
+--Our first step will be to run the following query:
+RESTORE FILELISTONLY FROM DISK='d:\Business_Data.bak';
+
+
+--Once we have these names, we will use the following query to restore a database to a new location.
+RESTORE DATABASE [appOwner]
+FROM DISK='d:\appOwner.bak'
+WITH
+MOVE 'Business_Data' TO 'D:\TSQL\Business_Data.mdf',
+MOVE 'Business_Data_log' TO 'D:\TSQL\Business_Data_log.ldf';
