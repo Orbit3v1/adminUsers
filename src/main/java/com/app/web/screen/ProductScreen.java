ea8b9c43a1ab221166f1204ac42fabc89b271970
ayaroslavtsev <duke007@ukr.net>
2016-09-16 16:27:04 +0300

    test with primefaces

--- src/main/java/com/app/web/screen/ProductScreen.java
+++ src/main/java/com/app/web/screen/ProductScreen.java
@@ -5,19 +5,20 @@ import com.app.data.entity.Product;
 import com.app.data.entity.Role;
 import com.app.utils.AppUtil;
 import com.app.utils.SessionUtil;
+import org.primefaces.event.CellEditEvent;
 import org.primefaces.model.DefaultTreeNode;
 import org.primefaces.model.TreeNode;
 import org.springframework.context.annotation.Scope;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 @Named("productScreen")
 @Scope("view")
@@ -28,6 +29,7 @@ public class ProductScreen {
 
     private TreeNode root;
     private Product entity;
+    private Map<Product, TreeNode> connector;
 
     @PostConstruct
     public void init() {
@@ -42,12 +44,37 @@ public class ProductScreen {
     }
 
     private void initRoot(){
-        root = new DefaultTreeNode(entity);
-        List<TreeNode> roots = new ArrayList<>();
-        TreeNode root1 = new DefaultTreeNode(entity.getSubordinates().get(0),root );
-        TreeNode root2 = new DefaultTreeNode(entity.getSubordinates().get(1),root1 );
-        for(Product p : entity.getSubordinates()){
-            roots.add(new DefaultTreeNode(p, root));
+        root = new DefaultTreeNode();
+        connector = new HashMap<>();
+        TreeNode tn = new DefaultTreeNode(entity, root);
+        connector.put(entity, tn);
+        initSubordinates(entity, tn);
+    }
+
+    @Transactional
+    private void initSubordinates(Product product, TreeNode parentNode){
+        product = em.find(Product.class, product.getId());
+        for(Product p : product.getSubordinates()){
+            TreeNode tn = new DefaultTreeNode(p, parentNode);
+            connector.put(p, tn);
+            initSubordinates(p, tn);
+        }
+    }
+
+    public void onCellEdit(CellEditEvent event) {
+        Object oldValue = event.getOldValue();
+        Object newValue = event.getNewValue();
+
+        if(newValue != null && !newValue.equals(oldValue)) {
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Cell Changed", "Old: " + oldValue + ", New:" + newValue);
+            FacesContext.getCurrentInstance().addMessage(null, msg);
+        }
+    }
+
+    @Transactional
+    public void save(){
+        for(Product p : connector.keySet()){
+            p =  em.merge(p);
         }
     }
 
