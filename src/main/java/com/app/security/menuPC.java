a4c9a4006de15890763f319c2e261161d5090e93
ayaroslavtsev <duke007@ukr.net>
2016-10-25 12:02:09 +0300

    refactor security class

--- src/main/java/com/app/security/menuPC.java
+++ src/main/java/com/app/security/menuPC.java
@@ -1,7 +1,48 @@
 package com.app.security;
 
-/**
- * Created by ayaroslavtsev on 25.10.2016.
- */
-public class menuPC {
+import com.app.data.entity.PrivilegeAction;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.app.security.Security.hasAnyPrivilegeAction;
+
+public class menuPC implements PrivilegeChecker {
+    @Override
+    public Map<String, Boolean> getPrivileges() {
+        Map<String, Boolean> userPA = new HashMap<>();
+
+        userPA.put("graphMenuR", hasAnyPrivilegeAction(
+                        new PrivilegeAction("graphMenu", "READ"))
+        );
+
+        userPA.put("adminMenuR", hasAnyPrivilegeAction(
+                        new PrivilegeAction("adminMenu", "READ"))
+        );
+
+        userPA.put("calcMenuR", hasAnyPrivilegeAction(
+                        new PrivilegeAction("calcMenu", "READ"))
+        );
+
+        userPA.put("adminMenuPersonR", hasAnyPrivilegeAction(
+                        new PrivilegeAction("adminMenuPerson", "READ"))
+        );
+
+        userPA.put("adminMenuRoleR", hasAnyPrivilegeAction(
+                        new PrivilegeAction("adminMenuRole", "READ"))
+        );
+
+        userPA.put("adminMenuNomenclatureR", hasAnyPrivilegeAction(
+                        new PrivilegeAction("adminMenuNomenclature", "READ"))
+        );
+
+        userPA.put("graphicMenuProductionR", hasAnyPrivilegeAction(
+                        new PrivilegeAction("graphicMenuProduction", "READ"))
+        );
+        userPA.put("graphicMenuSpecificationR", hasAnyPrivilegeAction(
+                        new PrivilegeAction("graphicMenuSpecification", "READ"))
+        );
+
+        return userPA;
+    }
 }
